4cdc26e66dab5f18d6968e334c9e2507fabc17c1
adimiz1 <adi.mizrahi@cloudinary.com>
2022-07-07 12:26:18 +0300

    Remove redundant file

--- test/cloudinary_dart_test.dart
+++ test/cloudinary_dart_test.dart
@@ -1,12 +1,8 @@
-import 'package:cloudinary_dart/src/config/cloudinary.dart';
 import 'package:cloudinary_dart/src/config/cloudinary_config.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    test('Test Cloudinary with URL string', () {
-      Cloudinary cloudinary = Cloudinary.withStringUrl('cloudinary://123456123456123:3Sf3FAdasa2easdFGDS3afADFS2@cloudname');
-    });
     test('Config with URL', () {
       CloudinaryConfig config = CloudinaryConfig.fromUri('cloudinary://123456123456123:3Sf3FAdasa2easdFGDS3afADFS2@cloudname?shorten=true&cname=custom.domain.com');
 
