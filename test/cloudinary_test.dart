540ffbcede12c3ef5ee2dc71241cc01ebaf888a0
adimiz1 <adi.mizrahi@cloudinary.com>
2022-07-17 12:17:21 +0300

    Add Cloudinary tests file

---
+++ test/cloudinary_test.dart
@@ -0,0 +1,22 @@
+import 'package:cloudinary_dart/src/cloudinary.dart';
+import 'package:cloudinary_dart/src/config/cloudinary_config.dart';
+import 'package:test/test.dart';
+import 'dart:io';
+
+void main() {
+  group('Cloud Config Tests', () {
+    test('Test Cloudinary initialization with valid url', () {
+        Cloudinary cloudinary = Cloudinary.withStringUrl("cloudinary://123456123456123:3Sf3FAdasa2easdFGDS3afADFS2@cloudname?shorten=true&cname=custom.domain.com");
+        assert("cloudname" == cloudinary.config.cloudConfig.cloudName);
+        assert("123456123456123" == cloudinary.config.cloudConfig.apiKey);
+        assert("3Sf3FAdasa2easdFGDS3afADFS2" == cloudinary.config.cloudConfig.apiSecret);
+    });
+    
+    test('Test Cloudinary initialization with configuration', () {
+      Cloudinary cloudinary = Cloudinary.withConfiguration(CloudinaryConfig.fromUri("cloudinary://123456123456123:3Sf3FAdasa2easdFGDS3afADFS2@cloudname?shorten=true&cname=custom.domain.com"));
+      assert("cloudname" == cloudinary.config.cloudConfig.cloudName);
+      assert("123456123456123" == cloudinary.config.cloudConfig.apiKey);
+      assert("3Sf3FAdasa2easdFGDS3afADFS2" == cloudinary.config.cloudConfig.apiSecret);
+    });
+  });
+}
