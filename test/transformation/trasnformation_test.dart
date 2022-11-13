697b824a48b2bf871e6d540ea7a90c4edbf4de54
adimiz1 <adi.mizrahi@cloudinary.com>
2022-11-13 18:04:50 +0200

    Add more tests

--- test/transformation/trasnformation_test.dart
+++ test/transformation/trasnformation_test.dart
@@ -1,3 +1,5 @@
+import 'package:cloudinary_dart/transformation/delivery/delivery_actions.dart';
+import 'package:cloudinary_dart/transformation/resize/resize.dart';
 import 'package:cloudinary_dart/transformation/rotate.dart';
 import 'package:cloudinary_dart/transformation/transformation.dart';
 import 'package:test/scaffolding.dart';
@@ -6,10 +8,19 @@ import '../tests_utils.dart';
 
 void main() {
   test('Test successful rotate formatting', () {
-    assert('a_50' == (Transformation()..rotate(Rotate.byAngle(50))).toString());
-    assert('a_50' == (Transformation()..rotate(Rotate(50))).toString());
+    cldAssert('a_50', (Transformation()..rotate(Rotate.byAngle(50))));
+    cldAssert('a_50', (Transformation()..rotate(Rotate(50))));
     cldAssert("a_-50", Transformation().rotate(Rotate.byAngle(-50)));
     cldAssert("a_hflip",
         Transformation().rotate(Rotate.mode(RotationMode.horizontalFlip())));
+  });
+  test('Test successful resize transformation formatting', () {
+    cldAssert('c_scale,w_500', Transformation() ..resize(Resize.scale() ..width(500)));
+  });
+  test('Test successful delivery transformation formatting', () {
+    cldAssert('q_auto', Transformation() ..delivery(Quality(Quality.auto)));
+  });
+  test('Test successful effect transformation formatting', () {
+    // cldAssert('q_auto', Transformation() ..);
   });
 }
