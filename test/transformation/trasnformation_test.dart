9daf071ccdc3472936b491543f6a7bfed4fd7896
adimiz1 <95848801+adimiz1@users.noreply.github.com>
2022-11-13 21:46:38 +0200

    Add `Transformation` tests

--- test/transformation/trasnformation_test.dart
+++ test/transformation/trasnformation_test.dart
@@ -1,3 +1,7 @@
+import 'package:cloudinary_dart/transformation/adjust/adjust.dart';
+import 'package:cloudinary_dart/transformation/delivery/delivery_actions.dart';
+import 'package:cloudinary_dart/transformation/effect/effect.dart';
+import 'package:cloudinary_dart/transformation/resize/resize.dart';
 import 'package:cloudinary_dart/transformation/rotate.dart';
 import 'package:cloudinary_dart/transformation/transformation.dart';
 import 'package:test/scaffolding.dart';
@@ -6,10 +10,25 @@ import '../tests_utils.dart';
 
 void main() {
   test('Test successful rotate formatting', () {
-    assert('a_50' == (Transformation()..rotate(Rotate.byAngle(50))).toString());
-    assert('a_50' == (Transformation()..rotate(Rotate(50))).toString());
+    cldAssert('a_50', (Transformation()..rotate(Rotate.byAngle(50))));
+    cldAssert('a_50', (Transformation()..rotate(Rotate(50))));
     cldAssert("a_-50", Transformation().rotate(Rotate.byAngle(-50)));
     cldAssert("a_hflip",
         Transformation().rotate(Rotate.mode(RotationMode.horizontalFlip())));
+  });
+  test('Test successful resize transformation formatting', () {
+    cldAssert('c_scale,w_500', Transformation() ..resize(Resize.scale() ..width(500)));
+  });
+  test('Test successful delivery transformation formatting', () {
+    cldAssert('q_auto', Transformation() ..delivery(Quality(Quality.auto)));
+  });
+  test('Test successful effect transformation formatting', () {
+     cldAssert('e_sepia:50', Transformation() ..effect(Effect.sepia(50)));
+  });
+  test('Test successful adjust transformation formatting', () {
+    cldAssert('e_hue:30', Transformation() ..adjust(Adjust.hue(30)));
+  });
+  test('Test successful generic transformation formatting', () {
+    cldAssert('a_0', Transformation() ..generic('a_0'));
   });
 }
