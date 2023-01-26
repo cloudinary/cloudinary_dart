dd92493e37f71f38aa1391b23f2e2ab3be1d59a8
adimiz1 <adi.mizrahi@cloudinary.com>
2023-01-26 11:33:53 +0200

    Started to work on upload api

---
+++ packages/uploader/lib/http/network_creator.dart
@@ -0,0 +1,22 @@
+import 'package:cloudinary_dart/http/network_options.dart';
+import 'package:dio/dio.dart';
+
+class NetworkCreator {
+  static var shared = NetworkCreator();
+  Dio _client = Dio();
+
+  Future<Response> request(
+      {required BaseClientGenerator route, NetworkOptions? options}) {
+    return _client.fetch(RequestOptions(
+        baseUrl: route.baseURL,
+        method: route.method,
+        path: route.path,
+        queryParameters: route.queryParameters,
+        data: route.body,
+        sendTimeout: route.sendTimeout,
+        receiveTimeout: route.sendTimeout,
+        onReceiveProgress: options?.onReceiveProgress,
+        validateStatus: (statusCode) => (statusCode! >= HttpStatus.ok &&
+            statusCode <= HttpStatus.multipleChoices)));
+  }
+}
---
+++ packages/uploader/lib/http/network_executer.dart
@@ -0,0 +1,42 @@
+class NetworkExecuter {
+  static var shared = NetworkExecuter();
+
+  bool debugMode = true;
+
+  Future<Result<K, NetworkError>> execute<T extends BaseNetworkModel, K>(
+      {required BaseClientGenerator route,
+      required T responseType,
+      NetworkOptions? options}) async {
+    if (debugMode) print(route);
+
+    // Check Network Connectivity
+    if (await NetworkConnectivity.status) {
+      try {
+        var response =
+            await NetworkCreator.shared.request(route: route, options: options);
+        var data = NetworkDecoding.shared
+            .decode<T, K>(response: response, responseType: responseType);
+        return Result.success(data);
+
+        // NETWORK ERROR
+      } on DioError catch (diorError) {
+        if (debugMode)
+          print("$route => ${NetworkError.request(error: diorError)}");
+        return Result.failure(NetworkError.request(error: diorError));
+
+        // TYPE ERROR
+      } on TypeError catch (e) {
+        if (debugMode)
+          print("$route => ${NetworkError.type(error: e.toString())}");
+        return Result.failure(NetworkError.type(error: e.toString()));
+      }
+
+      // No Internet Connection
+    } else {
+      if (debugMode)
+        print(NetworkError.connectivity(message: 'No Internet Connection'));
+      return Result.failure(
+          NetworkError.connectivity(message: 'No Internet Connection'));
+    }
+  }
+}
