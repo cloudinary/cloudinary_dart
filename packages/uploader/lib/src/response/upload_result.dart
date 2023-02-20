851021cad375821a057c54fe4b99a44621da7fb5
adimiz1 <adi.mizrahi@cloudinary.com>
2023-02-20 11:05:42 +0200

    Add missing upload result params

--- packages/uploader/lib/src/response/upload_result.dart
+++ packages/uploader/lib/src/response/upload_result.dart
@@ -1,4 +1,5 @@
 import '../request/model/params/access_control_rule.dart';
+import '../request/model/params/coordinates.dart';
 import '../request/model/params/responsive_breakpoint.dart';
 
 class UploadResult {
@@ -20,18 +21,77 @@ class UploadResult {
   String? secureUrl;
   String? accessMode;
   String? originalFilename;
-  // List<ResultColor>? colors;
-  // Map<String, List<ResultColor>>? predominant;
-  // List<ResultEager>? eager;
+  List<ResultColor>? colors;
+  Map<String, List<ResultColor>>? predominant;
+  List<ResultEager>? eager;
   String? status;
-  // Map<String, Coordinates>? coordinates;
-  // List<ResultModeration>? moderation;
+  Map<String, Coordinates>? coordinates;
+  List<ResultModeration>? moderation;
    List<ResponsiveBreakpoint>? responsiveBreakpoints;
    List<AccessControlRule>? accessControl;
-  // ResultQualityAnalysis? qualityAnalysis;
-  // ResultCinemagraphAnalysis? cinemagraphAnalysis;
+   ResultQualityAnalysis? qualityAnalysis;
+   ResultCinemagraphAnalysis? cinemagraphAnalysis;
   String? deleteToken;
-  // ResultContext? context;
+   ResultContext? context;
   bool? done;
-  // ResultAccessibilityAnalysis? accessibilityAnalysis;
+  ResultAccessibilityAnalysis? accessibilityAnalysis;
+}
+
+class ResultCinemagraphAnalysis {
+  double score;
+
+  ResultCinemagraphAnalysis(this.score);
+}
+
+class ResultColor {
+  String color;
+  double percent;
+
+  ResultColor(this.color, this.percent);
+}
+
+class ResultContext {
+  Map<String, String>? custom;
+
+  ResultContext(this.custom);
+}
+
+class ResultEager {
+  String? transformation;
+  int? width;
+  int? height;
+  int? bytes;
+  String? format;
+  String? url;
+  String? secureUrl;
+
+  ResultEager({this.transformation, this.width, this.height, this.bytes, this.format, this.url, this.secureUrl});
+}
+
+class ResultModeration {
+  String kind;
+  String status;
+
+  ResultModeration(this.kind, this.status);
+}
+
+class ResultQualityAnalysis {
+  double focus;
+
+  ResultQualityAnalysis(this.focus);
+}
+
+class ResultAccessibilityAnalysis {
+  ResultColorblindAccessibilityScore colorblindAccessibilityAnalysis;
+  double colorblindAccessibilityScore;
+
+  ResultAccessibilityAnalysis(this.colorblindAccessibilityAnalysis, this.colorblindAccessibilityScore);
+}
+
+class ResultColorblindAccessibilityScore {
+  double distinctEdges;
+  double distinctColors;
+  List<String> mostIndistinctPair;
+
+  ResultColorblindAccessibilityScore(this.distinctEdges, this.distinctColors, this.mostIndistinctPair);
 }
