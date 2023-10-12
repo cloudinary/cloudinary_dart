d567a43a4647fca028aac4068c68f5fc233ece44
adimiz1 <adi.mizrahi@cloudinary.com>
2023-10-12 16:20:35 +0300

    Fix tests

--- url_gen/test/analytics_test.dart
+++ url_gen/test/analytics_test.dart
@@ -9,19 +9,19 @@ void main() {
     test('Should successfully generate analytics string', () {
       var analytics = Analytics.fromParameters("1.24.0", "12.0", "17.0")
           .generateAnalyticsString();
-      cldAssert("CAOAlhAMZAR0", analytics);
+      cldAssert("CARAlhAMZAR0", analytics);
     });
     test('Should successfully generate analytics string', () {
       var analytics = Analytics.fromParameters("1.24.0-beta.6", "12.0", "17.0")
           .generateAnalyticsString();
-      cldAssert("CAOAlhAMZAR0", analytics);
+      cldAssert("CARAlhAMZAR0", analytics);
     });
     test(
         'Test sucessfully generate analytics string with patch for tech version',
         () {
       var analytics = Analytics.fromParameters("1.24.0", "12.0.1", "17.0")
           .generateAnalyticsString();
-      cldAssert("CAOAlhAMZAR0", analytics);
+      cldAssert("CARAlhAMZAR0", analytics);
     });
   });
 }
